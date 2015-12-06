6a209c6581b03c6f05c29c75f82873d9ae1adda8
Michael Gauland <michael.gauland@canterbury.ac.nz>
2015-12-07 11:48:22 +1300

    Start expanding interpreter.

--- read_test.py
+++ read_test.py
@@ -1,6 +1,8 @@
 #! /usr/bin/python3
 
 import pyslat
+import numpy as np
+import matplotlib.pyplot as plt
 
 command_list = {
     'FUNC': 'Create a function: FUNC ID TYPE PARAMS',
@@ -17,11 +19,13 @@ def usage(which=None):
 
 functions = dict()
 
-
+def line_error(line):
+    print("Error in line: ", line)
 
 try:
     while True:
-        command = input().split()
+        line = input()
+        command = line.split()
         if len(command) > 0:
             cmd = command[0].upper()
             if command[0]=='FUNC':
@@ -29,8 +33,8 @@ try:
                     flavours = {
                         'NLH': pyslat.FUNCTION_TYPE.NLH,
                         'PLC': pyslat.FUNCTION_TYPE.PLC,
-                        'LIN': pyslat.FUNCTION_TYPE.LIN,
-                        'LOGLOG': pyslat.FUNCTION_TYPE.LOGLOG
+                        #'LIN': pyslat.FUNCTION_TYPE.LIN,
+                        #'LOGLOG': pyslat.FUNCTION_TYPE.LOGLOG
                     }
 
                     id = command[1]
@@ -44,15 +48,59 @@ try:
                         if type:
                             functions[id] = pyslat.factory(flavour, params)
                         else:
+                            line_error(line)
+                            print("Error creating function")
                             usage("FUNC")
                 else:
+                    line_error(line)
+                    print("Too few arguments")
                     usage("FUNC")
+            elif command[0]=='TABLE':
+                start, stop, increment = map(lambda s: float(s), command[2].split(":"))
+                main_title, x_title, y_title = command[3].split(":")
+
+                try:
+                    f = functions[command[1]]
+                    print("\n", main_title)
+                    print(x_title, ", ", y_title)
+                    for x in np.arange(start, stop+increment, increment):
+                        print(x, ", ", f.ValueAt(x))
+                except KeyError:
+                    line_error(line)
+                    print("Undefined function")
+            elif command[0]=='PLOT':
+                try:
+                    f = functions[command[1]]
+                    start, stop, increment = map(lambda s: float(s), command[3].split(":"))
+                    x_data = np.arange(start, stop+increment, increment)
+                    y_data = list(map(f.ValueAt, x_data))
+                    
+                    if command[2]=="LIN":
+                        plt.plot(x_data, y_data)
+                    elif command[2]=="LOG":
+                        plt.loglog(x_data, y_data)
+                    else:
+                        raise ValueError(' '.join(['Unrecognised plot type:', command[2]])) 
+
+                    main_title, x_title, y_title = command[4].split(":")
+                    plt.title(main_title)
+                    plt.xlabel(x_title)
+                    plt.ylabel(y_title)
+                    
+                    if len(command) > 5:
+                        plt.savefig(command[5])
+                    else:
+                        plt.show()
+                except KeyError:
+                    line_error(line)
+                    print("Undefined function")
             elif command[0]=='IM':
-                print("intensity measure")
+                line_error(line)
+                print("intensity measure not implemented")
             else:
+                line_error(line)
+                printf("Unrecognised command")
                 usage(cmd)
-        else:
-            usage()
             
 except EOFError:
     print("Goodbye!")
