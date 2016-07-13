196dd8a7949b9107b50b962baea27ce960363f7d
Michael Gauland <michael.gauland@canterbury.ac.nz>
2016-07-14 09:44:28 +1200

    Start adding metrics to caching mechanism.

--- caching.h
+++ caching.h
@@ -28,20 +28,29 @@ namespace SLAT {
             std::unordered_map<V, T> cache;
             bool cache_active;
         public:
-            CachedFunction() {};
+            CachedFunction() {
+                hits = 0;
+                total_calls = 0;
+            };
             CachedFunction(std::function<T (V)> base_func, bool activate_cache=true) { 
                 cache_active = activate_cache;
+                hits = 0;
+                total_calls = 0;
                 func = base_func; 
                 Add_Cache(this, [this] (void) { 
                         this->ClearCache(); });
             };
             ~CachedFunction() {
                 Remove_Cache(this);
+                std::cout << "Destroying CachedFunction: " << hits << " hits of " << total_calls << " calls." << std::endl;
             }
             T operator()(V v) { 
+                total_calls++;
                 if (cache_active) {
                     if (cache.count(v) == 0) {
                         cache[v] = func(v); 
+                    } else {
+                        hits++;
                     }
                     return cache[v];
                 } else {
@@ -51,6 +60,9 @@ namespace SLAT {
             void ClearCache(void) {
                 cache.clear(); 
             };
+        private:
+            int hits;
+            int total_calls;
         };
 
         template <class T> class CachedValue {
@@ -58,19 +70,30 @@ namespace SLAT {
             std::function<T (void)> func;
             T cached_value;
             bool cache_valid;
+            int hits, total_calls;
         public:
-            CachedValue() : cache_valid(false) {};
+        CachedValue() : cache_valid(false) { 
+                total_calls = 0;
+                hits = 0;
+            };
             CachedValue(std::function<T (void)> base_func) { 
                 cache_valid = false;
+                total_calls = 0;
+                hits = 0;
                 func = base_func; 
                 Add_Cache(this, [this] (void) { 
                         this->ClearCache(); });
             };
-            ~CachedValue() {};
+            ~CachedValue() {
+                std::cout << "Destroying cache: " << hits << " hits of " << total_calls << " calls." << std::endl;
+            };
             T operator()(void) { 
+                total_calls++;
                 if (!cache_valid) {
                     cached_value = func();
                     cache_valid = true;
+                } else {
+                    hits++;
                 }
                 return cached_value;
             }
