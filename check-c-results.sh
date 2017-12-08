42314f4753da339365cc9844408c8154bbaadd34
Michael Gauland <michael.gauland@canterbury.ac.nz>
2017-12-08 13:02:59 +1300

    Add regression test to Makefile.

--- check-c-results.sh
+++ check-c-results.sh
@@ -2,15 +2,15 @@
 result=0
 for f in parser/example2/c-results/*; do
     #echo $f;
-    ./compare-values.awk $f ${f/c-results/results}
+    ./compare-values.awk $f ${f/c-results/expected-results}
     if [[ $? -ne 0 ]]; then
 	echo Error in  $(basename $f)
 	result=-1
     fi
 done
 
-for f in parser/example2/results//*; do
-    if [[ ! -e ${f/results/c-results} ]]; then
+for f in parser/example2/expected-results/*; do
+    if [[ ! -e ${f/expected-results/c-results} ]]; then
 	echo missing $(basename $f); 
 	result=-1
     fi;
